6e4645fc68fe1e7cde2ae717c3be6655af66148c
haminji <haminji@turnincode.cafe24.com>
2019-05-29 20:53:24 +0900

    thread indexing and mutex identifing

--- interpositioning/mymutex.c
+++ interpositioning/mymutex.c
@@ -11,51 +11,112 @@
 #include <sys/types.h>
 #include <sys/syscall.h>
 
+#define TRUE 1
+#define FALSE 0
+#define NUMOFMUTEX 100
+#define NUMOFTHREAD 10
+
+//static pid_t threads[NUMOFTHREAD];
+static pthread_t threads[NUMOFTHREAD];
+//static LockStack * locksperthread[10]; 
+static int head = 0;
+
+static int graph[NUMOFMUTEX][NUMOFMUTEX] = {
+	[0 ... NUMOFMUTEX-1] = { [0 ... NUMOFMUTEX-1] = 0 }
+};
+
+int visit[NUMOFMUTEX];
+
+int
+is_cycle_dfs(int node)
+{
+    visit[node] = 1;
+    if(node < NUMOFMUTEX) {
+        for(int i=0; i<NUMOFMUTEX; i++) {
+            if(graph[node][i] == 1) {
+                if(visit[i] == 1)
+                    return 1;
+                if(is_cycle_dfs(i) == 1)
+                    return 1;
+            }
+        }
+    }
+}
+
 void
-mutex_record(pthread_mutex_t * mutex)
+my_mk_edge()
 {
 
 }
+
+void
+my_rm_edge()
+{
+
+}
+
+
 
 
 int
 pthread_mutex_lock(pthread_mutex_t * mutex)
 {
-	pid_t ctid = syscall(SYS_gettid);
-	printf("thread: %d", ctid);
+//    pid_t ctid = syscall(SYS_gettid);
+    pthread_t ctid = pthread_self();
+    int ctindex = -1;
+    for(int i=0; i<NUMOFTHREAD; i++){
+        if(threads[i] == ctid){
+            ctindex = i;
+        }
+    }
+    if(ctindex == -1) {
+        ctindex = head;
+        threads[head] = ctid;
+        head++;
+    }
+   // printf("thread: %d index: %d", ctid, ctindex);
 
-	int (*pthread_mutex_lockp)(pthread_mutex_t * mutex);
-	char * error;
+    int (*pthread_mutex_lockp)(pthread_mutex_t * mutex);
+    char * error;
 	
-	pthread_mutex_lockp = dlsym(RTLD_NEXT, "pthread_mutex_lock");
-	if ((error = dlerror()) != 0x0)
-		exit(1);
-	int tmp = pthread_mutex_lockp(mutex);
+    pthread_mutex_lockp = dlsym(RTLD_NEXT, "pthread_mutex_lock");
+    if ((error = dlerror()) != 0x0)
+        exit(1);
+    int tmp = pthread_mutex_lockp(mutex);
 
-	char buf[50];
-	printf("mutex_lock : %p\n", mutex);
-	fputs(buf, stderr);
+    char buf[50];
+    printf("thread: %d index: %d", ctid, ctindex);
+    printf("@@mutex_lock : %p\n", mutex);
+    fputs(buf, stderr);
 
-	return tmp ;
+    return tmp ;
 }
 
 int
 pthread_mutex_unlock(pthread_mutex_t * mutex)
 {
-	pid_t ctid = syscall(SYS_gettid);
-	printf("thread: %d", ctid);
+//    pid_t ctid = syscall(SYS_gettid);
+    pthread_t ctid = pthread_self();
+    int ctindex;
+    for(int i=0; i<NUMOFTHREAD; i++){
+        if(threads[i] == ctid){
+            ctindex = i;
+        }
+    }
+//    printf("thread: %d", ctid);
 
-	int (*pthread_mutex_unlockp)(pthread_mutex_t * mutex);
-	char * error;
+    int (*pthread_mutex_unlockp)(pthread_mutex_t * mutex);
+    char * error;
 
-	pthread_mutex_unlockp = dlsym(RTLD_NEXT, "pthread_mutex_unlock");
-	if ((error = dlerror()) != 0x0)
-		exit(1);
-	int tmp = pthread_mutex_unlockp(mutex);
+    pthread_mutex_unlockp = dlsym(RTLD_NEXT, "pthread_mutex_unlock");
+    if ((error = dlerror()) != 0x0)
+        exit(1);
+    int tmp = pthread_mutex_unlockp(mutex);
 
-	char buf[50];
-	printf("mutex_unlock : %p\n", mutex);
-	fputs(buf, stderr);
+    char buf[50];
+    printf("thread: %d index: %d", ctid, ctindex);
+    printf("mutex_unlock : %p\n", mutex);
+    fputs(buf, stderr);
 
-	return tmp;
+    return tmp;
 }
